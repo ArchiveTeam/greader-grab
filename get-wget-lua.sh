777d5108e11fd0ae3839b1b4fdbfdb6c14858392
Ivan Kozik <ivan@ludios.org>
2013-05-18 23:20:47 +0000

    Verify sha1 of tarball

--- get-wget-lua.sh
+++ get-wget-lua.sh
@@ -3,6 +3,8 @@
 # This script downloads and compiles wget-lua.
 #
 
+set -e
+
 # first, try to detect gnutls or openssl
 CONFIGURE_SSL_OPT=""
 if builtin type -p pkg-config &>/dev/null
@@ -20,11 +22,18 @@ fi
 
 TARVERSION=1.14.lua.20130427-92d2
 TARFILE=wget-$TARVERSION.tar.bz2
+EXPECTED_SHA1=00705ce59e3cb14dee3c4b93c51377199cf2e2e8
 TARDIR=wget-$TARVERSION
 
 rm -rf $TARFILE $TARDIR/
 
 wget http://warriorhq.archiveteam.org/downloads/wget-lua/$TARFILE
+if [ "$(sha1sum $TARFILE | awk '{print $1}')" = "$EXPECTED_SHA1" ]; then
+  echo "sha1sum OK for $TARFILE"
+else
+  echo "sha1sum BAD for $TARFILE"
+  exit
+fi
 tar xjf $TARFILE
 cd $TARDIR/
 if ./configure $CONFIGURE_SSL_OPT --disable-nls && make && src/wget -V | grep -q lua
