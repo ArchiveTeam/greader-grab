ac2f94b587c98740e3d6c2e7a5a29394e6e533a6
Ivan Kozik <ivan@ludios.org>
2013-05-23 08:40:51 +0000

    Fix whitespace

--- pipeline.py
+++ pipeline.py
@@ -105,17 +105,20 @@ def find_executable(name, version, paths):
 # WGET_LUA will be set to the first path that
 # 1. does not crash with --version, and
 # 2. prints the required version string
-WGET_LUA = find_executable("Wget+Lua",
-				   [ "GNU Wget 1.14.lua.20130120-8476",
-				     "GNU Wget 1.14.lua.20130407-1f1d",
-				     "GNU Wget 1.14.lua.20130427-92d2" ],
-				   [ "./wget-lua",
-				     "./wget-lua-warrior",
-				     "./wget-lua-local",
-				     "../wget-lua",
-				     "../../wget-lua",
-				     "/home/warrior/wget-lua",
-				     "/usr/bin/wget-lua" ])
+WGET_LUA = find_executable("Wget+Lua", [
+		"GNU Wget 1.14.lua.20130120-8476",
+		"GNU Wget 1.14.lua.20130407-1f1d",
+		"GNU Wget 1.14.lua.20130427-92d2"
+	], [
+		"./wget-lua",
+		"./wget-lua-warrior",
+		"./wget-lua-local",
+		"../wget-lua",
+		"../../wget-lua",
+		"/home/warrior/wget-lua",
+		"/usr/bin/wget-lua"
+	]
+)
 
 if not WGET_LUA:
 	raise Exception("No usable Wget+Lua found.")
@@ -147,14 +150,14 @@ class PrepareDirectories(SimpleTask):
 	  A task that creates temporary directories and initializes filenames.
 
 	  It initializes these directories, based on the previously set item_name:
-	    item["item_dir"] = "%{data_dir}/%{item_name}"
-	    item["warc_file_base"] = "%{warc_prefix}-%{item_name}-%{timestamp}"
+		item["item_dir"] = "%{data_dir}/%{item_name}"
+		item["warc_file_base"] = "%{warc_prefix}-%{item_name}-%{timestamp}"
 
 	  These attributes are used in the following tasks, e.g., the Wget call.
 
 	  * set warc_prefix to the project name.
 	  * item["data_dir"] is set by the environment: it points to a working
-	    directory reserved for this item.
+		directory reserved for this item.
 	  * use item["item_dir"] for temporary files
 	  """
 	def __init__(self, warc_prefix):
@@ -163,7 +166,7 @@ class PrepareDirectories(SimpleTask):
 
 	def process(self, item):
 		item_name = item["item_name"]
-		dirname = "/".join(( item["data_dir"], item_name ))
+		dirname = "/".join((item["data_dir"], item_name))
 
 		if os.path.isdir(dirname):
 			shutil.rmtree(dirname)
@@ -186,7 +189,7 @@ class MoveFiles(SimpleTask):
 
 	def process(self, item):
 		os.rename("%(item_dir)s/%(warc_file_base)s.warc.gz" % item,
-			    "%(data_dir)s/%(warc_file_base)s.warc.gz" % item)
+				"%(data_dir)s/%(warc_file_base)s.warc.gz" % item)
 
 		shutil.rmtree("%(item_dir)s" % item)
 
@@ -199,11 +202,11 @@ class MoveFiles(SimpleTask):
 # This will be shown in the warrior management panel. The logo should not
 # be too big. The deadline is optional.
 project = Project(
-	title = "Google Reader",
-	project_html = """
-    <h2>Google Reader <span class="links"><a href="http://www.google.com/reader/">Website</a> &middot; <a
+	title="Google Reader",
+	project_html="""
+	<h2>Google Reader <span class="links"><a href="http://www.google.com/reader/">Website</a> &middot; <a
 href="http://tracker.archiveteam.org/greader/">Leaderboard</a></span></h2>
-    <p><i>Google Reader</i> is closing July 1st, 2013</p>
+	<p><i>Google Reader</i> is closing July 1st, 2013</p>
   """
 )
 
@@ -238,41 +241,39 @@ pipeline = Pipeline(
 	#
 	# the ItemInterpolation() objects are resolved during runtime
 	# (when there is an Item with values that can be added to the strings)
-	WgetDownload([ WGET_LUA,
-			   "-U", ItemInterpolation("%(user_agent)s"),
-			   "-nv",
-			   "-o", ItemInterpolation("%(item_dir)s/wget.log"),
-			   "--no-check-certificate",
-			   "--output-document", ItemInterpolation("%(item_dir)s/wget.tmp"),
-			   "--truncate-output",
-			   "-e", "robots=off",
-			   "--rotate-dns",
-			   "--timeout", "60",
-			   "--tries", "20",
-			   "--waitretry", "5",
-			   "--lua-script", "greader.lua",
-			   "--warc-file", ItemInterpolation("%(item_dir)s/%(warc_file_base)s"),
-			   "--warc-header", "operator: Archive Team",
-			   "--warc-header", "greader-dld-script-version: " + VERSION,
-			   ItemInterpolation("http://%(item_name)s/")
-			 ],
-			 max_tries = 2,
-			 # check this: which Wget exit codes count as a success?
-			 accept_on_exit_code = [ 0, 8 ],
-			 ),
+	WgetDownload([
+			WGET_LUA,
+			"-U", ItemInterpolation("%(user_agent)s"),
+			"-nv",
+			"-o", ItemInterpolation("%(item_dir)s/wget.log"),
+			"--no-check-certificate",
+			"--output-document", ItemInterpolation("%(item_dir)s/wget.tmp"),
+			"--truncate-output",
+			"-e", "robots=off",
+			"--rotate-dns",
+			"--timeout", "60",
+			"--tries", "20",
+			"--waitretry", "5",
+			"--lua-script", "greader.lua",
+			"--warc-file", ItemInterpolation("%(item_dir)s/%(warc_file_base)s"),
+			"--warc-header", "operator: Archive Team",
+			"--warc-header", "greader-dld-script-version: " + VERSION,
+			ItemInterpolation("http://%(item_name)s/")
+		], max_tries=2, accept_on_exit_code=[0, 8], # which Wget exit codes count as a success?
+	),
 
 	# this will set the item["stats"] string that is sent to the tracker (see below)
 	PrepareStatsForTracker(
 		# there are a few normal values that need to be sent
-		defaults = { "downloader": downloader, "version": VERSION },
+		defaults={"downloader": downloader, "version": VERSION},
 		# this is used for the size counter on the tracker:
 		# the groups should correspond with the groups set configured on the tracker
-		file_groups = {
+		file_groups={
 			# there can be multiple groups with multiple files
 			# file sizes are measured per group
-			"data": [ ItemInterpolation("%(item_dir)s/%(warc_file_base)s.warc.gz") ]
+			"data": [ItemInterpolation("%(item_dir)s/%(warc_file_base)s.warc.gz")]
 		},
-		id_function = (lambda item: {"ua": item["user_agent"] })
+		id_function=(lambda item: {"ua": item["user_agent"] })
 	),
 
 	# remove the temporary files, move the warc file from
@@ -283,35 +284,37 @@ pipeline = Pipeline(
 	# that there is only one item uploading at a time
 	#
 	# the NumberConfigValue can be changed in the configuration panel
-	LimitConcurrent(NumberConfigValue(min=1, max=4, default="1", name="shared:rsync_threads", title="Rsync threads",
-						    description="The maximum number of concurrent uploads."),
-			    # this upload task asks the tracker for an upload target
-			    # this can be HTTP or rsync and can be changed in the tracker admin panel
-			    UploadWithTracker(
-				    "http://tracker.archiveteam.org/%s" % TRACKER_ID,
-				    downloader = downloader,
-				    version = VERSION,
-				    # list the files that should be uploaded.
-				    # this may include directory names.
-				    # note: HTTP uploads will only upload the first file on this list
-				    files = [
-					    ItemInterpolation("%(data_dir)s/%(warc_file_base)s.warc.gz")
-				    ],
-				    # the relative path for the rsync command
-				    # (this defines if the files are uploaded to a subdirectory on the server)
-				    rsync_target_source_path = ItemInterpolation("%(data_dir)s/"),
-				    # extra rsync parameters (probably standard)
-				    rsync_extra_args = [
-					    "--recursive",
-					    "--partial",
-					    "--partial-dir", ".rsync-tmp"
-				    ]
-			    ),
-			    ),
+	LimitConcurrent(
+		NumberConfigValue(
+			min=1, max=4, default="1", name="shared:rsync_threads", title="Rsync threads",
+			description="The maximum number of concurrent uploads."),
+		# this upload task asks the tracker for an upload target
+		# this can be HTTP or rsync and can be changed in the tracker admin panel
+		UploadWithTracker(
+			"http://tracker.archiveteam.org/%s" % TRACKER_ID,
+			downloader=downloader,
+			version=VERSION,
+			# list the files that should be uploaded.
+			# this may include directory names.
+			# note: HTTP uploads will only upload the first file on this list
+			files=[
+				ItemInterpolation("%(data_dir)s/%(warc_file_base)s.warc.gz")
+			],
+			# the relative path for the rsync command
+			# (this defines if the files are uploaded to a subdirectory on the server)
+			rsync_target_source_path=ItemInterpolation("%(data_dir)s/"),
+			# extra rsync parameters (probably standard)
+			rsync_extra_args=[
+				"--recursive",
+				"--partial",
+				"--partial-dir", ".rsync-tmp"
+			]
+		),
+	),
 
 	# if the item passed every task, notify the tracker and report the statistics
 	SendDoneToTracker(
-		tracker_url = "http://tracker.archiveteam.org/%s" % TRACKER_ID,
-		stats = ItemValue("stats")
+		tracker_url="http://tracker.archiveteam.org/%s" % TRACKER_ID,
+		stats=ItemValue("stats")
 	)
 )
