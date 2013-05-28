379591c5b1921d771bc95eca8d544db9d9ee6a72
Ivan Kozik <ivan@ludios.org>
2013-05-28 13:43:50 +0000

    Accept only SSL certs signed by Google's root CA, EquifaxSecureCA; improve docstring

--- pipeline.py
+++ pipeline.py
@@ -1,11 +1,33 @@
-# This file defines a seesaw pipeline for the ArchiveTeam Warrior.
-# It can also be run standalone:
-#
-#   pip install seesaw
-#   run-pipeline pipeline.py YOURNICKNAME
-#
-# (or run   run-pipeline --help   for more details)
-#
+r"""
+This file defines a seesaw pipeline for the ArchiveTeam Warrior.
+It can also be run standalone:
+
+   pip install --user seesaw
+   ~/.local/bin/run-pipeline pipeline.py YOURNICKNAME
+
+(or run   run-pipeline --help   for more details)
+
+---
+
+This pipeline relies on this code inserted into your universal-tracker redis database:
+
+$ redis-cli
+redis 127.0.0.1:6379> select 13
+OK
+redis 127.0.0.1:6379[13]> set greader:extra_parameters 'data["task_urls"] = (File.open("/home/greader-items/" + item[0...6] + "/" + item, "r") do |f| f.read end).split("\n").map {|encoded_url| "https://www.google.com/reader/api/0/stream/contents/feed/" + encoded_url + "?r=n&n=1000&hl=en&likes=true&comments=true&client=ArchiveTeam"}; data["user_agent"] = "Wget/1.14 ArchiveTeam"; data["wget_timeout"] = "60"; data["wget_tries"] = "20"; data["wget_waitretry"] = "5";'
+OK
+
+/home/greader-items/ (change the location) should have lists of
+urllib.quote_plus-encoded URLs with this directory layout:
+
+000000/0000000000
+000000/0000000001
+
+That is,
+[first 6 digits]/[all 10 digits]
+...
+"""
+
 import os
 import json
 import os.path
@@ -28,26 +50,9 @@ from seesaw.pipeline import Pipeline
 from seesaw.externalprocess import WgetDownload
 from seesaw.tracker import TrackerRequest, UploadWithTracker, SendDoneToTracker, PrepareStatsForTracker
 
-r"""
-This pipeline relies on this code inserted into your universal-tracker redis database:
-
-$ redis-cli
-redis 127.0.0.1:6379> select 13
-OK
-redis 127.0.0.1:6379[13]> set greader:extra_parameters 'data["task_urls"] = (File.open("/home/greader-items/" + item[0...6] + "/" + item, "r") do |f| f.read end).split("\n").map {|encoded_url| "https://www.google.com/reader/api/0/stream/contents/feed/" + encoded_url + "?r=n&n=1000&hl=en&likes=true&comments=true&client=ArchiveTeam"}; data["user_agent"] = "Wget/1.14 ArchiveTeam"; data["wget_timeout"] = "60"; data["wget_tries"] = "20"; data["wget_waitretry"] = "5";'
-OK
-
-/home/greader-items/ (change the location) should have lists of
-urllib.quote_plus-encoded URLs with this directory layout:
-
-000000/0000000000
-000000/0000000001
-000000/0000000002
-...
-000001/0000010000
-000001/0000010001
-...
-"""
+# run-pipeline changes the cwd to the directory containing pipeline.py, then
+# execs the contents of pipeline.py.
+SSL_CERT_DIR = os.path.join(os.getcwd(), "certs")
 
 
 class GetItemFromTracker(TrackerRequest):
@@ -280,6 +285,7 @@ pipeline = Pipeline(
 		],
 		max_tries=2,
 		accept_on_exit_code=[0, 8], # which Wget exit codes count as a success?
+		env=dict(SSL_CERT_DIR=SSL_CERT_DIR),
 		stdin_data_function=(lambda item: "\n".join(u.encode("utf-8") for u in item["task_urls"]) + "\n"),
 	),
 
