8125d15b7d569d15a283b32c13c1e3691a08cdf5
Ivan Kozik <ivan@ludios.org>
2013-05-31 00:34:11 +0000

    Grab the continuation without decoding JSON.  This should drastically reduce CPU and memory usage.

--- greader.lua
+++ greader.lua
@@ -2,16 +2,14 @@
 Based on https://github.com/ArchiveTeam/wget-lua-forum-scripts/blob/master/vbulletin.lua
 --]]
 
-read_file = function(file)
+read_file = function(file, amount)
   local f = io.open(file)
-  local data = f:read("*all")
+  local data = f:read(amount)
   f:close()
   return data
 end
 
 url_count = 0
-
-json = require("dkjson")
 
 url_with_continuation = function(url, continuation)
   assert(string.len(continuation) == 12, "continuation should be 12 bytes, was " .. string.len(continuation))
@@ -28,19 +26,14 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
     print(" - Downloaded "..url_count.." URLs")
   end
 
-  local page = read_file(file)
+  -- Read 32KB in case the feed has a really long title
+  local page = read_file(file, 32768)
   if string.sub(page, 0, 1) ~= "{" then
     -- page has no JSON (probably a 404 page)
     return {}
   end
 
-  local decoded, pos, error = json.decode(page)
-  if not decoded then
-    print("Could not decode JSON (unexpected!): " .. error)
-    return {}
-  end
-
-  local continuation = decoded["continuation"]
+  local continuation = string.match(page, '"continuation":"(C..........C)"')
   if continuation then
     return {{url=url_with_continuation(url, continuation), link_expect_html=0}}
   end
