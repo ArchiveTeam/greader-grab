5e93663a7bd7407113f503f607c0a9b36797bac4
Ivan Kozik <ivan@ludios.org>
2013-06-02 17:49:06 +0000

    Revert autoinstall scripts - this pull request was created before the instructions were fixed

--- ubuntu-autoinstall.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y build-essential lua5.1 liblua5.1-0-dev python python-setuptools python-dev git-core openssl libssl-dev python-pip rsync gcc make git screen
-pip install --user seesaw
-git clone https://github.com/ArchiveTeam/greader-grab
-cd greader-grab
-./get-wget-lua.sh
